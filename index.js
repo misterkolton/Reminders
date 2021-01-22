63e82bfcfa5eb77d09da5e1ac75b13756fef0a6e
kolton thompson <koltonthompson@koltons-MacBook-Pro.local>
2021-01-22 15:15:21 -0700

    minor updates

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const billInputElement = document.getElementById("reminderInput");
 const saveReminder = document.getElementById("saveReminder");
 const reminderOutput = document.querySelector(".reminderOutput");
-let reminder = "";
+let reminder = "fuck ass";
 
-function saveBill(e) {
+function newReminder(e) {
   reminder = e.target.value;
 }
 
-function getTotalBill() {
-  const calculatedBill = reminder;
-  reminderOutput.innerHTML = calculatedBill;
-}
+function getReminder() {
+  const displayReminder = reminder;
+  reminderOutput.innerHTML = displayReminder;
+  }
 
-billInputElement.addEventListener("change", saveBill);
-saveReminder.addEventListener("click", getTotalBill);
+billInputElement.addEventListener("change", newReminder);
+saveReminder.addEventListener("click", getReminder);
