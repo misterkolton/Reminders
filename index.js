74d243774de39a8ee8f28cb525a6609453873586
kolton thompson <koltonthompson@koltons-MacBook-Pro.local>
2021-01-27 12:26:08 -0700

    function for 'enter' key is working

--- index.js
+++ index.js
@@ -7,9 +7,16 @@ function addItem(){
     li.setAttribute('id',reminderInput.value);
     li.appendChild(document.createTextNode(reminderInput.value));
     ul.appendChild(li);
+    
 }
 
 function removeItem(){
    const item = document.getElementById(reminderInput.value);
     ul.removeChild(item);
 }
+
+document.getElementById('reminderInput').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        addItem()
+    }
+});
