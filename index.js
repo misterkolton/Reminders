baaa58ecc3efa1b9aa7d023b7b74f91f1d5f5a91
kolton thompson <koltonthompson@koltons-MacBook-Pro.local>
2021-01-27 14:34:09 -0700

    delete button is functional with removing item from index 0. also clearing the input field after event is working while delete still works also

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ function addItem(){
     li.setAttribute('id',reminderInput.value);
     li.appendChild(document.createTextNode(reminderInput.value));
     ul.appendChild(li);
-    
+    document.getElementById("reminderInput").value = "";
 }
 
 function removeItem(){
-   const item = document.getElementById(reminderInput.value);
-    ul.removeChild(item);
+//    const item = document.getElementById(reminderInput.value);
+//     ul.removeChild(item);
+const list = document.getElementById("reminder-list");   // Get the <ul> element with id="reminder-list"
+list.removeChild(list.childNodes[0]);           // Remove <ul>'s first child node (index 0)
 }
 
 document.getElementById('reminderInput').addEventListener('keypress', function (e) {
